bfdf89dd32e6e83456f0d9522201e9ec9d5e213b
pungkyhwa <pungky.hwa@gmail.com>
2023-11-15 10:43:03 +0700

    admin lte

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+@include('layouts.header')
+
+  <!-- Navbar -->
+  @include('layouts.navbar')
+  
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('layouts.sidebar')
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <?php ?>
+            <h1 class="m-0">{{ $title ?? '' }}</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item active">{{ $nm_menu ?? '' }}</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    @yield('content')
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  @include('layouts.footer')
