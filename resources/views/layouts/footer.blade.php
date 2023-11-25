c147d6cb4cbd5e793d65963c1ccadac302d10cb9
azkifairuz <azkiajmal@gmail.com>
2023-11-25 13:07:03 +0700

    intial project

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -49,5 +49,6 @@
 <script src="{{asset('AdminLTE')}}/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{asset('AdminLTE')}}/dist/js/pages/dashboard.js"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DasboardController;
+use App\Http\Controllers\ProduksiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/login',[DasboardController::class,'index']);
-Route::get('/dasboard',[DasboardController::class,'index']);
+// Route::get('/dasboard',[DasboardController::class,'index']);
+Route::get('/',[DasboardController::class,'index']);
+Route::get('/produksi',[ProduksiController::class,'index']);
