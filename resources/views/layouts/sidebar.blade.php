2e0e19a55c346a7195aa9367f19aee74eec72dd5
azkifairuz <azkiajmal@gmail.com>
2023-11-26 18:16:24 +0700

    feat: redirect if not authorize

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -34,12 +34,11 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="{{route('dasboard')}}" class="nav-link active">
+          <li class="nav-item ">
+            <a href="{{route('dasboard')}}" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
           </li>
